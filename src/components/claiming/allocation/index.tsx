0799981975f70ac5febf618986b34902fab24d97
Sotatek-KhangTa <khang.ta@sotatek.com>
2022-09-08 15:34:12 +0700

    fix bug line-chart

--- src/components/claiming/allocation/index.tsx
+++ src/components/claiming/allocation/index.tsx
@@ -54,6 +54,73 @@ export default function Allocation({ dataClaim, fetchListJoinClaim }: Props) {
   const infoClaimData = useSelector((s: any) => s.claimAction.data);
   const [lineChartData, setLineChartData] = useState<any>();
 
+  const handleLineChart = useCallback(async () => {
+    setLoadingTransaction(true);
+    const res = await getClaimList(dataClaim.id);
+    if (res.data) {
+      if (!isEmpty(res.data)) {
+        const clone = res.data
+          .sort(function (a: any, b: any) {
+            return (
+              new Date(b.created_at).valueOf() -
+              new Date(a.created_at).valueOf()
+            );
+          })
+          .map((el: any, index: number) => {
+            return {
+              value: el.amount,
+              name: moment(el.created_at).format("YYYY-MM-DD"),
+            };
+          });
+
+        const listDate = Array.from(Array(7).keys())?.map((el) => {
+          return {
+            name: moment(Date()).subtract(el, "d").format("YYYY-MM-DD"),
+            value: 0,
+          };
+        });
+
+        const listData = _.uniqBy([...clone, ...listDate], "name")
+          .sort(function (a: any, b: any) {
+            return new Date(b.name).valueOf() - new Date(a.name).valueOf();
+          })
+          .map((el: any) => {
+            const date = moment(el.name).date();
+            return { name: date, value: el.value };
+          });
+
+        listData.length = 7;
+
+        listData.reverse();
+        setLineChartData(listData);
+      } else {
+        const listDate = Array.from(Array(7).keys())
+          ?.map((el) => {
+            return {
+              name: moment(Date()).subtract(el, "d").format("YYYY-MM-DD"),
+              value: 0,
+            };
+          })
+          .sort(function (a: any, b: any) {
+            return new Date(b.name).valueOf() - new Date(a.name).valueOf();
+          })
+          .map((el: any) => {
+            const date = moment(el.name).date();
+            return { name: date, value: el.value };
+          });
+        listDate.reverse();
+        setLineChartData(listDate);
+      }
+    } else {
+      toast.error(res?.error.message);
+    }
+    setLoadingTransaction(false);
+  }, [dataClaim.id]);
+
+  useEffect(() => {
+    handleLineChart();
+  }, [handleLineChart]);
+
   const handleClaim = async (
     abi: any,
     contractAddress: string
@@ -122,6 +189,7 @@ export default function Allocation({ dataClaim, fetchListJoinClaim }: Props) {
       if (!time_out_claim) {
         toast.success("Successful transaction done");
         fetchListJoinClaim();
+        handleLineChart();
       } else {
         toast.error(
           "Transaction Pending. Please wait for transaction success and reload page"
@@ -140,73 +208,6 @@ export default function Allocation({ dataClaim, fetchListJoinClaim }: Props) {
   const isEmpty = (v: any) => {
     return Object.keys(v).length === 0;
   };
-
-  const handleLineChart = useCallback(async () => {
-    setLoadingTransaction(true);
-    const res = await getClaimList(dataClaim.id);
-    if (res.data) {
-      if (!isEmpty(res.data)) {
-        const clone = res.data
-          .sort(function (a: any, b: any) {
-            return (
-              new Date(b.created_at).valueOf() -
-              new Date(a.created_at).valueOf()
-            );
-          })
-          .map((el: any, index: number) => {
-            return {
-              value: el.amount,
-              name: moment(el.created_at).format("YYYY-MM-DD"),
-            };
-          });
-
-        const listDate = Array.from(Array(7).keys())?.map((el) => {
-          return {
-            name: moment(clone[0].name).subtract(el, "d").format("YYYY-MM-DD"),
-            value: 0,
-          };
-        });
-
-        const listData = _.uniqBy([...clone, ...listDate], "name")
-          .sort(function (a: any, b: any) {
-            return new Date(b.name).valueOf() - new Date(a.name).valueOf();
-          })
-          .map((el: any) => {
-            const date = moment(el.name).date();
-            return { name: date, value: el.value };
-          });
-
-        listData.length = 7;
-
-        listData.reverse();
-        setLineChartData(listData);
-      } else {
-        const listDate = Array.from(Array(7).keys())
-          ?.map((el) => {
-            return {
-              name: moment(Date()).subtract(el, "d").format("YYYY-MM-DD"),
-              value: 0,
-            };
-          })
-          .sort(function (a: any, b: any) {
-            return new Date(b.name).valueOf() - new Date(a.name).valueOf();
-          })
-          .map((el: any) => {
-            const date = moment(el.name).date();
-            return { name: date, value: el.value };
-          });
-        listDate.reverse();
-        setLineChartData(listDate);
-      }
-    } else {
-      toast.error(res?.error.message);
-    }
-    setLoadingTransaction(false);
-  }, [dataClaim.id]);
-
-  useEffect(() => {
-    handleLineChart();
-  }, [handleLineChart]);
 
   return (
     <>
