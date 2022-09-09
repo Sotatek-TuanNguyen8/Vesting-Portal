6f687a5fd0e50302c40fac1e62c31e425a20aa91
Sotatek TuanNguyen <tuan.nguyen8@sotatek.com>
2022-09-09 11:38:09 +0700

    check token valid

--- src/components/Auth/reset-password/index.tsx
+++ src/components/Auth/reset-password/index.tsx
@@ -109,11 +109,8 @@ export default function ResetPasswordPage() {
   return (
     <LayoutPass>
       <Loading open={isLoading} />
-      <div className={classes.reset}>
-        <Typography variant="h5" className={classes.title}>
-          Reset Password
-        </Typography>
-        {isCheckTokenValid ? (
+      {isCheckTokenValid ? (
+        <div className={classes.expired}>
           <div className={classes.container}>
             <Typography variant="h5">
               Reset password link has been expired.
@@ -123,175 +120,188 @@ export default function ResetPasswordPage() {
               <Button>Back to Log in</Button>
             </Link>
           </div>
-        ) : isSuccess ? (
-          <div className={classes.pwChange}>
-            <div className="content">
-              <p>Password Changed!</p>
-              <span>Your password has been changed successfully</span>
+        </div>
+      ) : (
+        <div className={classes.reset}>
+          <Typography variant="h5" className={classes.title}>
+            Reset Password
+          </Typography>
+          {isSuccess ? (
+            <div className={classes.pwChange}>
+              <div className="content">
+                <p>Password Changed!</p>
+                <span>Your password has been changed successfully</span>
+              </div>
+              <div className="actionBack">
+                <Link to="/sign-in">
+                  <Button>Back To Login</Button>
+                </Link>
+              </div>
             </div>
-            <div className="actionBack">
-              <Link to="/sign-in">
-                <Button>Back To Login</Button>
-              </Link>
-            </div>
-          </div>
-        ) : (
-          <form
-            onSubmit={handleSubmit((data) => handleResetPassword(data))}
-            className={classes.form}
-          >
-            <div className={classes.inputForm}>
-              <Controller
-                control={control}
-                name="password"
-                rules={{
-                  required: "This field is required",
-                  minLength: {
-                    value: 8,
-                    message: "Minimum is 8 characters",
-                  },
-                }}
-                render={({
-                  field: { value, onChange, ref },
-                  fieldState: { error },
-                }) => {
-                  return (
-                    <>
-                      <TextField
-                        id="adornment-password"
-                        type={showPassword ? "text" : "password"}
-                        value={removeMark(value)}
-                        autoComplete="off"
-                        onChange={onChangePassWork}
-                        label="Password"
-                        error={!!error?.message}
-                        InputProps={{
-                          endAdornment: (
-                            <InputAdornment position="end">
-                              <IconButton
-                                aria-label="toggle password visibility"
-                                onClick={() => setShowPassword((prev) => !prev)}
-                              >
-                                {showPassword ? (
-                                  <Visibility />
-                                ) : (
-                                  <VisibilityOff />
-                                )}
-                              </IconButton>
-                            </InputAdornment>
-                          ),
-                        }}
-                      />
-                      {error && error.message && (
-                        <p className={classes.inputErrorPass}>
-                          {error.message}
-                        </p>
-                      )}
-                      {watchPassword.length >= 8 && (
-                        <div className={classes.passwordLength}>
-                          <div
-                            className={
-                              stylePassWord === 1
-                                ? "weak"
-                                : stylePassWord === 2
-                                ? "normal"
-                                : "strong"
-                            }
-                          >
-                            <div className="line"></div>
+          ) : (
+            <form
+              onSubmit={handleSubmit((data) => handleResetPassword(data))}
+              className={classes.form}
+            >
+              <div className={classes.inputForm}>
+                <Controller
+                  control={control}
+                  name="password"
+                  rules={{
+                    required: "This field is required",
+                    minLength: {
+                      value: 8,
+                      message: "Minimum is 8 characters",
+                    },
+                  }}
+                  render={({
+                    field: { value, onChange, ref },
+                    fieldState: { error },
+                  }) => {
+                    return (
+                      <>
+                        <TextField
+                          id="adornment-password"
+                          type={showPassword ? "text" : "password"}
+                          value={removeMark(value)}
+                          autoComplete="off"
+                          onChange={onChangePassWork}
+                          label="Password"
+                          error={!!error?.message}
+                          InputProps={{
+                            endAdornment: (
+                              <InputAdornment position="end">
+                                <IconButton
+                                  aria-label="toggle password visibility"
+                                  onClick={() =>
+                                    setShowPassword((prev) => !prev)
+                                  }
+                                >
+                                  {showPassword ? (
+                                    <Visibility />
+                                  ) : (
+                                    <VisibilityOff />
+                                  )}
+                                </IconButton>
+                              </InputAdornment>
+                            ),
+                          }}
+                        />
+                        {error && error.message && (
+                          <p className={classes.inputErrorPass}>
+                            {error.message}
+                          </p>
+                        )}
+                        {watchPassword.length >= 8 && (
+                          <div className={classes.passwordLength}>
+                            <div
+                              className={
+                                stylePassWord === 1
+                                  ? "weak"
+                                  : stylePassWord === 2
+                                  ? "normal"
+                                  : "strong"
+                              }
+                            >
+                              <div className="line"></div>
+                            </div>
+                            <div
+                              className={clsx(
+                                stylePassWord < 2
+                                  ? "default"
+                                  : stylePassWord === 2
+                                  ? "normal"
+                                  : "strong"
+                              )}
+                            >
+                              <div className="line"></div>
+                            </div>
+                            <div
+                              className={clsx(
+                                stylePassWord < 3 ? "default" : "strong"
+                              )}
+                            >
+                              <div className="line"></div>
+                            </div>
                           </div>
-                          <div
-                            className={clsx(
-                              stylePassWord < 2
-                                ? "default"
-                                : stylePassWord === 2
-                                ? "normal"
-                                : "strong"
-                            )}
-                          >
-                            <div className="line"></div>
-                          </div>
-                          <div
-                            className={clsx(
-                              stylePassWord < 3 ? "default" : "strong"
-                            )}
-                          >
-                            <div className="line"></div>
-                          </div>
-                        </div>
-                      )}
-                      <Tooltip
-                        title="Your password must be 8 characters minimum and
+                        )}
+                        <Tooltip
+                          title="Your password must be 8 characters minimum and
 should contain lowercase letter, uppercase letter, 
 number and special character."
-                        arrow
-                      >
-                        <span style={{ marginTop: 7, display: "inline-block" }}>
-                          <ToolTipIcon />
-                        </span>
-                      </Tooltip>
-                    </>
-                  );
-                }}
-              />
-            </div>
-            <div className={classes.inputForm}>
-              <Controller
-                control={control}
-                name="confirm_password"
-                render={({
-                  field: { value, onChange, ref },
-                  fieldState: { error },
-                }) => {
-                  return (
-                    <>
-                      <TextField
-                        id="adornment-confirm-password"
-                        type={showPassword ? "text" : "password"}
-                        value={removeMark(value)}
-                        onChange={onChangeConfirmPassWork}
-                        label="Confirm Password"
-                        error={!!error?.message}
-                        InputProps={{
-                          endAdornment: (
-                            <InputAdornment position="end">
-                              <IconButton
-                                aria-label="toggle confirm password visibility"
-                                onClick={() => setShowPassword((prev) => !prev)}
-                              >
-                                {showPassword ? (
-                                  <Visibility />
-                                ) : (
-                                  <VisibilityOff />
-                                )}
-                              </IconButton>
-                            </InputAdornment>
-                          ),
-                        }}
-                      />
-                      {error && error.message && (
-                        <p className={classes.inputError}>{error.message}</p>
-                      )}
-                    </>
-                  );
-                }}
-              />
-            </div>
-            <div className={classes.action}>
-              <Link to="/sign-in" className="btnBack">
-                Back to login
-              </Link>
-              <button
-                type="submit"
-                className="btnContinue"
-                disabled={isLoading}
-              >
-                Continue
-              </button>
-            </div>
-          </form>
-        )}
-      </div>
+                          arrow
+                        >
+                          <span
+                            style={{ marginTop: 7, display: "inline-block" }}
+                          >
+                            <ToolTipIcon />
+                          </span>
+                        </Tooltip>
+                      </>
+                    );
+                  }}
+                />
+              </div>
+              <div className={classes.inputForm}>
+                <Controller
+                  control={control}
+                  name="confirm_password"
+                  render={({
+                    field: { value, onChange, ref },
+                    fieldState: { error },
+                  }) => {
+                    return (
+                      <>
+                        <TextField
+                          id="adornment-confirm-password"
+                          type={showPassword ? "text" : "password"}
+                          value={removeMark(value)}
+                          onChange={onChangeConfirmPassWork}
+                          label="Confirm Password"
+                          error={!!error?.message}
+                          InputProps={{
+                            endAdornment: (
+                              <InputAdornment position="end">
+                                <IconButton
+                                  aria-label="toggle confirm password visibility"
+                                  onClick={() =>
+                                    setShowPassword((prev) => !prev)
+                                  }
+                                >
+                                  {showPassword ? (
+                                    <Visibility />
+                                  ) : (
+                                    <VisibilityOff />
+                                  )}
+                                </IconButton>
+                              </InputAdornment>
+                            ),
+                          }}
+                        />
+                        {error && error.message && (
+                          <p className={classes.inputError}>{error.message}</p>
+                        )}
+                      </>
+                    );
+                  }}
+                />
+              </div>
+              <div className={classes.action}>
+                <Link to="/sign-in" className="btnBack">
+                  Back to login
+                </Link>
+                <button
+                  type="submit"
+                  className="btnContinue"
+                  disabled={isLoading}
+                >
+                  Continue
+                </button>
+              </div>
+            </form>
+          )}
+        </div>
+      )}
     </LayoutPass>
   );
 }
