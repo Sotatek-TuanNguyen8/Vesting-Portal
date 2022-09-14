e9c26d66af575ec359010805bb9f0a3c277c4b79
Sotatek-Tienpham <tien.pham@sotatek.com>
2022-09-14 17:36:07 +0700

    fix/bug

---
+++ src/components/hooks/useSocketFn.ts
@@ -0,0 +1,25 @@
+import { useEffect } from "react";
+
+import { useStableFn } from "./useStableFn";
+import { useSocket } from "./useSocket";
+import { SocketEvent } from "../../utils/types/socket";
+
+export function useSocketFn(
+  forEvent: SocketEvent,
+  callback: (...arg: any[]) => any,
+  enabled = true
+) {
+  const { registerListener } = useSocket();
+
+  const handler = useStableFn(callback);
+
+  useEffect(() => {
+    if (!enabled) return;
+    const cleanup = registerListener(forEvent, handler);
+
+    return () => {
+      cleanup();
+    };
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [registerListener, forEvent, enabled]);
+}
